3c881879199aae3b9517b087366110888267b5b9
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-07-08 13:36:19 +0100

    BAVL-235 fixing offset datetime issue spotted during testing with real release event in dev.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/common/LocalDateExt.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/common/LocalDateExt.kt
@@ -1,9 +1,14 @@
 package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.common
 
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 fun LocalDate.toMediumFormatStyle(): String = this.format(DateTimeFormatter.ofPattern("d MMM yyyy", Locale.ENGLISH))
 
 fun LocalDate.toIsoDate(): String = this.format(DateTimeFormatter.ISO_DATE)
+
+fun LocalDateTime.toOffsetString(): String =
+  DateTimeFormatter.ISO_OFFSET_DATE_TIME.format(this.atOffset(ZoneId.of("Europe/London").rules.getOffset(this)))
