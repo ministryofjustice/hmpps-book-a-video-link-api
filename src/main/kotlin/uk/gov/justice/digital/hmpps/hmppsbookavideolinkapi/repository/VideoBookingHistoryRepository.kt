b7edf28cf1374158251b0961ddcfca39bda2aa89
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-07-24 17:03:44 +0100

    BAVL-266 fixing query for bookings start date, should be looking at updates over create.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/repository/VideoBookingHistoryRepository.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/repository/VideoBookingHistoryRepository.kt
@@ -1,6 +1,8 @@
 package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.repository
 
+import jakarta.persistence.QueryHint
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.jpa.repository.QueryHints
 import org.springframework.stereotype.Repository
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.entity.VideoBookingHistory
 import java.time.LocalDate
@@ -8,6 +10,14 @@ import java.util.stream.Stream
 
 @Repository
 interface VideoBookingHistoryRepository : ReadOnlyRepository<VideoBookingHistory, Long> {
+
+  @QueryHints(
+    value = [
+      QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_FETCH_SIZE, value = "" + Integer.MAX_VALUE),
+      QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_CACHEABLE, value = "false"),
+      QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_READ_ONLY, value = "true"),
+    ],
+  )
   @Query(
     value = """
       FROM VideoBookingHistory vbh 
@@ -17,10 +27,20 @@ interface VideoBookingHistoryRepository : ReadOnlyRepository<VideoBookingHistory
   )
   fun findByDateOfBookingBetween(fromDate: LocalDate, toDate: LocalDate): Stream<VideoBookingHistory>
 
+  @QueryHints(
+    value = [
+      QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_FETCH_SIZE, value = "" + Integer.MAX_VALUE),
+      QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_CACHEABLE, value = "false"),
+      QueryHint(name = org.hibernate.jpa.HibernateHints.HINT_READ_ONLY, value = "true"),
+    ],
+  )
   @Query(
     value = """
-      FROM VideoBookingHistory vbh 
-      WHERE vbh.mainDate >= :fromDate and vbh.mainDate <= :toDate
+      FROM VideoBookingHistory vbh
+      LEFT JOIN VideoBookingHistory later on vbh.videoBookingId = later.videoBookingId and vbh.timestamp < later.timestamp
+      WHERE later.videoBookingId is null
+      AND   vbh.mainDate >= :fromDate and vbh.mainDate <= :toDate
+      AND   vbh.historyType != 'CANCEL'
       ORDER BY vbh.mainDate, vbh.mainStartTime
     """,
   )
