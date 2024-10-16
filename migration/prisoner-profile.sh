e72bb6ccf86291a11f74a6cf96408e47953e4902
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-10-16 13:48:33 +0100

    BAVL-383 fleshing out the prisoner profile script for switching rollout/rollback feature flags.

--- migration/prisoner-profile.sh
+++ migration/prisoner-profile.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
 
-echo "TODO - prisoner profile rollout script"
+ENV=$1
+ACTION=$2
+
+NAMESPACE="hmpps-prisoner-profile-$ENV"
+SECRETS_FILE="hmpps-prisoner-profile-secrets.yaml"
+
+if [ "$ACTION" = "out" ]; then
+  BVLS_TOGGLED_ON="true"
+elif [ "$ACTION" = "back" ]; then
+  BVLS_TOGGLED_ON="false"
+else
+  echo "Invalid action '$ACTION' for '$NAMESPACE', terminating process."
+  return 1 2> /dev/null || exit 1
+fi
+
+echo "Rolling $ACTION $NAMESPACE switches..."
+
+# Create the secrets YAML file
+cat <<EOF > $SECRETS_FILE
+apiVersion: v1
+kind: Secret
+metadata:
+  name: feature-toggles
+  namespace: $NAMESPACE
+type: Opaque
+stringData:
+  BOOK_A_VIDEO_LINK_FEATURE_TOGGLE_ENABLED: "$BVLS_TOGGLED_ON"
+EOF
+
+# Apply the secret
+kubectl apply -f ./$SECRETS_FILE
+echo "Applied secrets to $NAMESPACE"
+
+rm -f ./$SECRETS_FILE
+
+BOOK_A_VIDEO_LINK_FEATURE_TOGGLE_ENABLED=$(kubectl -n "$NAMESPACE" get secret feature-toggles -o jsonpath='{.data.BOOK_A_VIDEO_LINK_FEATURE_TOGGLE_ENABLED}' | base64 -d)
+
+echo "BOOK_A_VIDEO_LINK_FEATURE_TOGGLE_ENABLED=BOOK_A_VIDEO_LINK_FEATURE_TOGGLE_ENABLED"
+
+# Restart the deployment pods
+echo "Restarting deployment hmpps-prisoner-profile on namespace $NAMESPACE"
+kubectl -n "$NAMESPACE" rollout restart deployments/hmpps-prisoner-profile
