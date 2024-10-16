8476c9e3232425a47904f5633bdc73332339aafb
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-10-16 12:14:18 +0100

    BAVL-378 refactoring to only restart the deployments we are interested in instead of a blanket all. (#240)

--- migration/whereabouts-api.sh
+++ migration/whereabouts-api.sh
@@ -44,6 +44,6 @@ MIGRATE_EVENTS=$(kubectl -n "$NAMESPACE" get secret feature-toggles -o jsonpath=
 echo "FEATURE_LISTEN_FOR_COURT_EVENTS=$COURT_EVENTS"
 echo "FEATURE_SEND_VIDEO_BOOKING_MIGRATE_EVENTS=$MIGRATE_EVENTS"
 
-# Restart the pods
-echo "Restarting pods on namespace $NAMESPACE"
-kubectl -n "$NAMESPACE" rollout restart deploy
+# Restart the deployment pods
+echo "Restarting deployment whereabouts-api-generic-service on namespace $NAMESPACE"
+kubectl -n "$NAMESPACE" rollout restart deployments/whereabouts-api-generic-service
