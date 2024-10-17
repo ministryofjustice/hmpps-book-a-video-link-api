26c3537c24660548f211cce28f3ae9c683ce3ddb
Stephen McVeigh <smcveigh941@gmail.com>
2024-10-17 16:39:46 +0100

    Remove script

--- migration/remove-maintenance-screen.sh
+++ migration/remove-maintenance-screen.sh
@@ -3,9 +3,29 @@
 ENV=$1
 
 NAMESPACE="hmpps-book-a-video-link-$ENV"
+SECRETS_FILE="book-a-video-link-secrets.yaml"
 
-kubectl -n "$NAMESPACE" delete secret maintenance
-echo "Removed maintenance secret from $NAMESPACE"
+# Create the secrets YAML file
+cat <<EOF > $SECRETS_FILE
+apiVersion: v1
+kind: Secret
+metadata:
+  name: maintenance
+  namespace: $NAMESPACE
+type: Opaque
+stringData:
+  maintenance_mode: "false"
+EOF
+
+# Apply the secret
+kubectl apply -f ./$SECRETS_FILE
+echo "Applied secrets to $NAMESPACE"
+
+rm -f ./$SECRETS_FILE
+
+MAINTENANCE_MODE=$(kubectl -n "$NAMESPACE" get secret maintenance -o jsonpath='{.data.maintenance_mode}' | base64 -d)
+
+echo "maintenance_mode=$MAINTENANCE_MODE"
 
 # Restart the deployment pods
 echo "Restarting deployment the hmpps-book-a-video-link-ui pods on namespace $NAMESPACE"
