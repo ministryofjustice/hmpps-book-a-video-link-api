bd0acbd4030511b8de1c82b350e48a237e3aa524
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-10-18 15:00:09 +0100

    BAVL-258 refactoring telemetry code in preparation for adding metric based telemetry.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/service/TelemetryService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/service/TelemetryService.kt
@@ -5,19 +5,20 @@ import org.springframework.stereotype.Service
 
 @Service
 class TelemetryService(private val telemetryClient: TelemetryClient) {
-
   fun track(event: TelemetryEvent) {
-    telemetryClient.trackEvent(event.eventType.label, event.properties(), null)
+    when (event) {
+      is MetricTelemetryEvent -> telemetryClient.trackEvent(event.eventType, event.properties(), event.metrics())
+      is StandardTelemetryEvent -> telemetryClient.trackEvent(event.eventType, event.properties(), null)
+    }
   }
 }
 
-abstract class TelemetryEvent(val eventType: TelemetryEventType) {
+sealed class TelemetryEvent(val eventType: String) {
   abstract fun properties(): Map<String, String>
 }
 
-enum class TelemetryEventType(val label: String) {
-  MIGRATED_BOOKING_FAILURE("BVLS-migrated-booking-failure"),
-  MIGRATED_BOOKING_SUCCESS("BVLS-migrated-booking-success"),
-  JOB_FAILURE("BVLS-job-failure"),
-  JOB_SUCCESS("BVLS-job-success"),
+abstract class MetricTelemetryEvent(eventType: String) : TelemetryEvent(eventType) {
+  abstract fun metrics(): Map<String, Double>
 }
+
+abstract class StandardTelemetryEvent(eventType: String) : TelemetryEvent(eventType)
