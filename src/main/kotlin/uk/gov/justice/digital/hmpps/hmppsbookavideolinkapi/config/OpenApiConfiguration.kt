6bcd44430f5ec68d53c7aa3130421d6245b6c5bf
Stephen McVeigh <smcveigh941@gmail.com>
2024-10-25 13:34:52 +0100

    Improve swagger docs by listing roles and protection measures

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/config/OpenApiConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/config/OpenApiConfiguration.kt
@@ -3,17 +3,32 @@ package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.config
 import io.swagger.v3.core.util.PrimitiveType
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.Operation
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
 import io.swagger.v3.oas.models.security.SecurityRequirement
 import io.swagger.v3.oas.models.security.SecurityScheme
 import jakarta.annotation.PostConstruct
+import org.springdoc.core.customizers.OperationCustomizer
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.info.BuildProperties
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.expression.BeanFactoryResolver
+import org.springframework.expression.spel.SpelEvaluationException
+import org.springframework.expression.spel.standard.SpelExpressionParser
+import org.springframework.expression.spel.support.StandardEvaluationContext
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.method.HandlerMethod
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.common.asListOfType
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.resource.ProtectedByIngress
 
 @Configuration
 class OpenApiConfiguration(buildProperties: BuildProperties) {
+
+  @Autowired
+  private lateinit var context: ApplicationContext
 
   private val version: String = buildProperties.version
 
@@ -42,6 +57,46 @@ class OpenApiConfiguration(buildProperties: BuildProperties) {
         ),
     )
     .addSecurityItem(SecurityRequirement().addList("bearer-jwt", listOf("read", "write")))
+
+  @Bean
+  fun authorizationCustomizer(): OperationCustomizer {
+    return OperationCustomizer { operation: Operation, handlerMethod: HandlerMethod ->
+      val preAuthorizeValue = handlerMethod.getMethodAnnotation(PreAuthorize::class.java)?.value
+        ?: handlerMethod.beanType.getAnnotation(PreAuthorize::class.java)?.value
+
+      val protectedByIngress = handlerMethod.getMethodAnnotation(ProtectedByIngress::class.java)
+        ?: handlerMethod.beanType.getAnnotation(ProtectedByIngress::class.java)
+
+      preAuthorizeValue?.let { expression ->
+        val spelParser = SpelExpressionParser()
+        val parsedExpression = spelParser.parseExpression(expression)
+        val spelContext = StandardEvaluationContext().apply {
+          beanResolver = BeanFactoryResolver(context)
+          setRootObject(object {
+            fun hasRole(role: String) = listOf(role)
+            fun hasAnyRole(vararg roles: String) = roles.toList()
+          })
+        }
+
+        val roles = try {
+          (parsedExpression.getValue(spelContext) as List<*>).asListOfType<String>()
+        } catch (e: SpelEvaluationException) {
+          emptyList()
+        }
+
+        if (roles.isNotEmpty()) {
+          val rolesDescription = roles.joinToString(prefix = "* ", separator = "\n* ")
+          operation.description = "${operation.description ?: ""}\n\nRequires one of the following roles:\n$rolesDescription"
+        }
+      }
+
+      protectedByIngress?.let {
+        operation.description = "${operation.description ?: ""}\n\nThis endpoint can only be accessed from within the ingress. Requests from elsewhere will result in a 401 response code."
+      }
+
+      operation
+    }
+  }
 
   @PostConstruct
   fun enableLocalTimePrimitiveType() {
