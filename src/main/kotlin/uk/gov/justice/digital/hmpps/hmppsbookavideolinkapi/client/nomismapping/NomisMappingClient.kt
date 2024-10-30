69675583bda3b3b77aff2299d79d15f1f4e4717a
Stephen McVeigh <smcveigh941@gmail.com>
2024-10-30 14:28:15 +0000

    BAVL-401: Database changes

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/client/nomismapping/NomisMappingClient.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.client.nomismapping
+
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
+import reactor.core.publisher.Mono
+import java.util.UUID
+
+@Component
+class NomisMappingClient(private val nomisMappingApiWebClient: WebClient) {
+  companion object {
+    private val log = LoggerFactory.getLogger(this::class.java)
+  }
+
+  fun getNomisLocationMappingBy(internalLocationId: Long): NomisDpsLocationMapping? = nomisMappingApiWebClient
+    .get()
+    .uri("/api/locations/nomis/{id}", internalLocationId)
+    .retrieve()
+    .bodyToMono(NomisDpsLocationMapping::class.java)
+    .doOnError { error -> log.info("Error looking up internal location mapping by nomis location id $internalLocationId in mapping service", error) }
+    .onErrorResume(WebClientResponseException.NotFound::class.java) { Mono.empty() }
+    .block()
+
+  fun getNomisLocationMappingBy(dpsLocationId: UUID): NomisDpsLocationMapping? = nomisMappingApiWebClient
+    .get()
+    .uri("/api/locations/dps/{id}", dpsLocationId)
+    .retrieve()
+    .bodyToMono(NomisDpsLocationMapping::class.java)
+    .doOnError { error -> log.info("Error looking up internal location mapping by dps location id $dpsLocationId in mapping service", error) }
+    .onErrorResume(WebClientResponseException.NotFound::class.java) { Mono.empty() }
+    .block()
+}
+
+data class NomisDpsLocationMapping(
+  val dpsLocationId: UUID,
+  val nomisLocationId: Long,
+)
--- src/main/resources/migrations/common/V2024.10.08.1__baseline_data.sql
+++ src/main/resources/migrations/common/V2024.10.08.1__baseline_data.sql
@@ -626,6 +626,20 @@ values (1,'WNI', 'Werrington (HMYOI)', false, null, 'TIM', current_timestamp),
        (110,'TSI', 'Thameside (HMP)', true, null, 'TIM', current_timestamp),
        (111,'PBI', 'Peterborough (HMP)', true, null, 'TIM', current_timestamp),
        (112,'BZI', 'Bronzefield (HMP)', true, null, 'TIM', current_timestamp),
-       (113,'AGI', 'Askham Grange (HMP & YOI)', false, null, 'TIM', current_timestamp);
+       (113,'AGI', 'Askham Grange (HMP & YOI)', false, null, 'TIM', current_timestamp),
+       (114,'ACI', 'Altcourse (HMP & YOI)', false, null, 'TIM', current_timestamp),
+       (115,'ASI', 'Ashfield (HMP)', false, null, 'TIM', current_timestamp),
+       (116,'DGI', 'Dovegate (HMP)', false, null, 'TIM', current_timestamp),
+       (117,'DNI', 'Doncaster (HMP)', false, null, 'TIM', current_timestamp),
+       (118,'FBI', 'Forest Bank (HMP)', false, null, 'TIM', current_timestamp),
+       (119,'FEI', 'Fosse Way (HMP)', false, null, 'TIM', current_timestamp),
+       (120,'FWI', 'Five Wells (HMP)', false, null, 'TIM', current_timestamp),
+       (121,'LGI', 'Lowdham Grange (HMP)', false, null, 'TIM', current_timestamp),
+       (122,'NLI', 'Northumberland (HMP)', false, null, 'TIM', current_timestamp),
+       (123,'OWI', 'Oakwood (HMP)', false, null, 'TIM', current_timestamp),
+       (124,'PFI', 'Peterborough (HMP & YOI)', false, null, 'TIM', current_timestamp),
+       (125,'PRI', 'Parc (HMP)', false, null, 'TIM', current_timestamp),
+       (126,'PYI', 'Parc (YOI)', false, null, 'TIM', current_timestamp),
+       (127,'RHI', 'Rye Hill (HMP)', false, null, 'TIM', current_timestamp);
 
-alter sequence if exists prison_prison_id_seq restart with 114;
+alter sequence if exists prison_prison_id_seq restart with 128;
