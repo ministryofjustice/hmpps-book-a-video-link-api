4eab102636560b493ef4705b01dd55d7d7938025
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-11-14 13:28:30 +0000

    BAVL-412 and postgres test container so can run tests without having to run docker compose file.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/integration/config/FeatureSwitchesTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/integration/config/FeatureSwitchesTest.kt
@@ -1,21 +1,20 @@
-package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.config
+package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.integration.config
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.config.Feature
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.config.FeatureSwitches
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.isBool
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.integration.IntegrationTestBase
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.events.InboundEventsListener
 import uk.gov.justice.hmpps.sqs.HmppsQueueService
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-@ActiveProfiles("test")
-class FeatureSwitchesTest {
+class FeatureSwitchesTest : IntegrationTestBase() {
 
   // Beans are mocked out so as not to interfere with the running of the tests. We do not want/need them on the context.
   @MockBean
