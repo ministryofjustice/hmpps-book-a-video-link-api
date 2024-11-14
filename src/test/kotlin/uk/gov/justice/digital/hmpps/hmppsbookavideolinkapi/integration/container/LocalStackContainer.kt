e90c07e017d50ccc81e7502044d082cd43500549
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-11-14 16:54:20 +0000

    BAVL-413 alter approach checking for published messages so can run integration test in the CI pipeline. (#270)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/integration/container/LocalStackContainer.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/integration/container/LocalStackContainer.kt
@@ -36,8 +36,7 @@ object LocalStackContainer {
 
     return LocalStackContainer(DockerImageName.parse("localstack/localstack").withTag("3"))
       .apply {
-        withServices(SNS)
-        withServices(SQS)
+        withServices(SNS, SQS)
         withEnv("HOSTNAME_EXTERNAL", "localhost")
         withEnv("DEFAULT_REGION", "eu-west-2")
         waitingFor(Wait.forLogMessage(".*Ready.*", 1))
