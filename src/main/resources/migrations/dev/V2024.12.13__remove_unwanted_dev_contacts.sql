ca0ba7535675c200c4cc9e86a9602d093cd0ccc6
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2024-12-13 16:23:22 +0000

    BAVL-532 remove fake dev contacts, causes errors in our dev logs.

---
+++ src/main/resources/migrations/dev/V2024.12.13__remove_unwanted_dev_contacts.sql
@@ -0,0 +1,3 @@
+delete from court_contact where email in ('j@j.com', 'b@b.com', 'c@c.com');
+delete from probation_team_contact where email in ('t@t.com', 'm@m.com', 's@s.com');
+delete from prison_contact where email in ('p@p.com', 'g@g.com', 'z@z.com', 'a@a.com', 'r@r.com');
