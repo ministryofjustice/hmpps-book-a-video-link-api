c50cc07f215f4b9b502cadd311ce1340972b44e1
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-01-15 13:17:26 +0000

    BAVL-525 join for meeting/hearing type should have been on the booking_history table and not the video_booking table.

--- src/test/resources/integration-test-data/seed-court-events-by-hearing-date-data-extract.sql
+++ src/test/resources/integration-test-data/seed-court-events-by-hearing-date-data-extract.sql
@@ -5,7 +5,7 @@ insert into prison_appointment (video_booking_id, prison_id, prisoner_number, ap
 values (-1000, 1, 'ABCDEF', 'VLB_COURT_MAIN', 'comments about the hearing', '926d8f38-7149-4fda-b51f-85abcbcb0d00'::uuid, '2100-07-24', '12:00', '13:00');
 
 insert into booking_history(booking_history_id, video_booking_id, history_type, court_id, hearing_type, comments, created_by, created_time)
-values (-1000, -1000, 'CREATE', 1, 'APPEAL','comments about the hearing', 'court_user', '2024-07-24T01:00:00');
+values (-1000, -1000, 'CREATE', 1, 'TRIBUNAL','comments about the hearing', 'court_user', '2024-07-24T01:00:00');
 
 insert into booking_history_appointment (booking_history_id, prison_code, prisoner_number, appointment_date, appointment_type, prison_location_id, start_time, end_time)
 values (-1000, 'PVI', 'ABCDEF', '2100-07-24', 'VLB_COURT_MAIN', '926d8f38-7149-4fda-b51f-85abcbcb0d00'::uuid, '12:00', '13:00');
@@ -23,7 +23,7 @@ insert into prison_appointment (video_booking_id, prison_id, prisoner_number, ap
 values (-2000, 1, 'ABCDEF', 'VLB_COURT_MAIN', 'comments about the hearing', '926d8f38-7149-4fda-b51f-85abcbcb0d00'::uuid, '2100-07-24', '12:00', '13:00');
 
 insert into booking_history(booking_history_id, video_booking_id, history_type, court_id, hearing_type, comments, created_by, created_time)
-values (-2000, -2000, 'CREATE',409, 'APPEAL','comments about the hearing', 'court_user', '2024-07-24T01:00:00');
+values (-2000, -2000, 'CREATE',409, 'TRIBUNAL','comments about the hearing', 'court_user', '2024-07-24T01:00:00');
 
 insert into booking_history_appointment (booking_history_id, prison_code, prisoner_number, appointment_date, appointment_type, prison_location_id, start_time, end_time)
 values (-2000, 'PVI', 'ABCDEF', '2100-07-24', 'VLB_COURT_MAIN', '926d8f38-7149-4fda-b51f-85abcbcb0d00'::uuid, '12:00', '13:00');
