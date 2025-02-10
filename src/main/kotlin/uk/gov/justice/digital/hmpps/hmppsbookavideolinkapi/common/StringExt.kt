b7b28779d6f857fa72fc1272d30be22a45802724
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-02-10 12:11:13 +0000

    BAVL-611 new entity to support the capture of additional booking details (for the updated probation journey).

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/common/StringExt.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/common/StringExt.kt
@@ -1,9 +1,18 @@
 package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.common
 
+import com.google.i18n.phonenumbers.PhoneNumberUtil
 import java.time.OffsetDateTime
+import java.util.Locale
+
+private val ukCountryCode: String = Locale.UK.country
+private val phoneNumberUtil: PhoneNumberUtil = PhoneNumberUtil.getInstance()
 
 fun String.isEmail() = this.matches(
   Regex("[a-zA-Z0-9\\+\\.\\_\\%\\-\\+]{1,256}\\@[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}(\\.[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25})+"),
 )
 
 fun String.toOffsetDateTime(): OffsetDateTime = OffsetDateTime.parse(this)
+
+fun String.isUkPhoneNumber(): Boolean = runCatching {
+  phoneNumberUtil.parse(this, ukCountryCode).let { phoneNumberUtil.isValidNumber(it) }
+}.getOrElse { false }
