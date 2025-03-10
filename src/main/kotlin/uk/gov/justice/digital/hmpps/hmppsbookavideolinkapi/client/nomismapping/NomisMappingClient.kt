30aeecc9b6f00a036feafd35e7579761581f3b6b
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-03-10 13:09:55 +0000

    BAVL-676 using bulk nomis mapping location lookup endpoint now that it is available in favour of multiple single calls. (#399)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/client/nomismapping/NomisMappingClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/client/nomismapping/NomisMappingClient.kt
@@ -1,11 +1,16 @@
 package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.client.nomismapping
 
 import org.slf4j.LoggerFactory
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
 import reactor.core.publisher.Mono
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.config.CacheConfiguration
 import java.util.UUID
+
+inline fun <reified T> typeReference() = object : ParameterizedTypeReference<T>() {}
 
 @Component
 class NomisMappingClient(private val nomisMappingApiWebClient: WebClient) {
@@ -13,7 +18,15 @@ class NomisMappingClient(private val nomisMappingApiWebClient: WebClient) {
     private val log = LoggerFactory.getLogger(this::class.java)
   }
 
-  fun getNomisLocationMappingsBy(dpsLocationIds: Collection<UUID>): Collection<NomisDpsLocationMapping> = dpsLocationIds.mapNotNull { getNomisLocationMappingBy(it) }
+  @Cacheable(CacheConfiguration.NOMIS_MAPPING_CACHE_NAME)
+  fun getNomisLocationMappingsBy(dpsLocationIds: DpsLocationsIds) = nomisMappingApiWebClient
+    .post()
+    .uri("/api/locations/dps")
+    .bodyValue(dpsLocationIds)
+    .retrieve()
+    .bodyToMono(typeReference<Collection<NomisDpsLocationMapping>>())
+    .onErrorResume(WebClientResponseException.NotFound::class.java) { Mono.empty() }
+    .block() ?: emptyList()
 
   fun getNomisLocationMappingBy(dpsLocationId: UUID): NomisDpsLocationMapping? = nomisMappingApiWebClient
     .get()
@@ -23,6 +36,20 @@ class NomisMappingClient(private val nomisMappingApiWebClient: WebClient) {
     .doOnError { error -> log.info("Error looking up internal location mapping by dps location id $dpsLocationId in mapping service", error) }
     .onErrorResume(WebClientResponseException.NotFound::class.java) { Mono.empty() }
     .block()
+}
+
+class DpsLocationsIds(locationIds: Collection<UUID>) {
+  val locationIds = locationIds.sorted()
+  override fun equals(other: Any?): Boolean {
+    if (this === other) return true
+    if (javaClass != other?.javaClass) return false
+
+    other as DpsLocationsIds
+
+    return locationIds == other.locationIds
+  }
+
+  override fun hashCode() = locationIds.hashCode()
 }
 
 data class NomisDpsLocationMapping(
--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/config/CacheConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/config/CacheConfiguration.kt
@@ -22,6 +22,9 @@ class CacheConfiguration {
     const val VIDEO_LINK_LOCATIONS_CACHE_NAME: String = "video_link_locations"
     const val NON_RESIDENTIAL_LOCATIONS_CACHE_NAME: String = "non_residential_locations"
     const val ROLLED_OUT_PRISONS_CACHE_NAME = "rolled_out_prisons"
+    const val NOMIS_MAPPING_CACHE_NAME = "nomis_mapping"
+    const val TWO = 2L
+    const val FIVE = 5L
   }
 
   @Bean
@@ -29,23 +32,30 @@ class CacheConfiguration {
     VIDEO_LINK_LOCATIONS_CACHE_NAME,
     NON_RESIDENTIAL_LOCATIONS_CACHE_NAME,
     ROLLED_OUT_PRISONS_CACHE_NAME,
+    NOMIS_MAPPING_CACHE_NAME,
   )
 
   @CacheEvict(value = [VIDEO_LINK_LOCATIONS_CACHE_NAME], allEntries = true)
-  @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
+  @Scheduled(fixedDelay = FIVE, timeUnit = TimeUnit.MINUTES)
   fun cacheEvictVideoLinkLocations() {
-    log.info("Evicting cache: $VIDEO_LINK_LOCATIONS_CACHE_NAME after 5 mins")
+    log.info("Evicting cache: $VIDEO_LINK_LOCATIONS_CACHE_NAME after $FIVE mins")
   }
 
   @CacheEvict(value = [NON_RESIDENTIAL_LOCATIONS_CACHE_NAME], allEntries = true)
-  @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
+  @Scheduled(fixedDelay = FIVE, timeUnit = TimeUnit.MINUTES)
   fun cacheEvictNonResidentialLocations() {
-    log.info("Evicting cache: $NON_RESIDENTIAL_LOCATIONS_CACHE_NAME after 5 mins")
+    log.info("Evicting cache: $NON_RESIDENTIAL_LOCATIONS_CACHE_NAME after $FIVE mins")
   }
 
   @CacheEvict(value = [ROLLED_OUT_PRISONS_CACHE_NAME], allEntries = true)
-  @Scheduled(fixedDelay = 5, timeUnit = TimeUnit.MINUTES)
+  @Scheduled(fixedDelay = FIVE, timeUnit = TimeUnit.MINUTES)
   fun cacheEvictRolledOutPrisons() {
-    log.info("Evicting cache: $ROLLED_OUT_PRISONS_CACHE_NAME after 5 mins")
+    log.info("Evicting cache: $ROLLED_OUT_PRISONS_CACHE_NAME after $FIVE mins")
+  }
+
+  @CacheEvict(value = [NOMIS_MAPPING_CACHE_NAME], allEntries = true)
+  @Scheduled(fixedDelay = TWO, timeUnit = TimeUnit.HOURS)
+  fun cacheEvictNomisMapping() {
+    log.info("Evicting cache: $NOMIS_MAPPING_CACHE_NAME after $TWO hours")
   }
 }
