af2f45315902b1eabb787a2c059e095902e6a295
Stephen McVeigh <smcveigh941@gmail.com>
2025-03-14 12:45:44 +0000

    Add a utility endpoint to publish a domain event on the SNS topic

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/resource/JobController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/resource/JobController.kt
@@ -2,13 +2,18 @@ package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.resource
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.model.request.PublishEventUtilityModel
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.events.DomainEventType
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.events.OutboundEventsService
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.jobs.JobTriggerService
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.jobs.JobType
 
@@ -23,9 +28,20 @@ import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.jobs.JobType
 @RequestMapping(value = ["job-admin"], produces = [MediaType.TEXT_PLAIN_VALUE])
 class JobController(
   private val jobTriggerService: JobTriggerService,
+  private val outboundEventsService: OutboundEventsService,
 ) {
   @Operation(summary = "Endpoint to trigger a job, perhaps from a cron schedule.")
   @PostMapping(path = ["/run/{jobName}"])
   @ResponseStatus(HttpStatus.OK)
   fun runJob(@PathVariable("jobName") jobName: JobType) = jobTriggerService.run(jobName).resultMessage
+
+  @Operation(summary = "Endpoint to publish an event to the domain events SNS topic.")
+  @PostMapping(path = ["/publish/{domainEventType}"])
+  @ResponseStatus(HttpStatus.OK)
+  fun publishDomainEvent(
+    @PathVariable("domainEventType") domainEventType: DomainEventType,
+    @Valid
+    @RequestBody
+    publishEventUtilityModel: PublishEventUtilityModel,
+  ) = publishEventUtilityModel.identifiers!!.forEach{ outboundEventsService.send(domainEventType, it) }
 }
