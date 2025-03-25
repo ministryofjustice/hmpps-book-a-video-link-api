2f0cc8a2c19fd43507f128333f7fc233c3cdd459
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-03-25 08:56:33 +0000

    BAVL-663 removed scheduled row constraint from domain but missed in service layer. (#426)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/service/locations/administration/CreateLocationScheduleService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/service/locations/administration/CreateLocationScheduleService.kt
@@ -4,7 +4,6 @@ import jakarta.persistence.EntityNotFoundException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.entity.LocationScheduleUsage
-import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.entity.LocationUsage.SCHEDULE
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.model.request.CreateRoomScheduleRequest
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.repository.LocationAttributeRepository
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.ExternalUser
@@ -19,10 +18,6 @@ class CreateLocationScheduleService(
   fun create(dpsLocationId: UUID, request: CreateRoomScheduleRequest, createdBy: ExternalUser) = run {
     val locationAttribute = locationAttributeRepository.findByDpsLocationId(dpsLocationId)
       ?: throw EntityNotFoundException("Location attribute with DPS location ID $dpsLocationId not found")
-
-    require(locationAttribute.isLocationUsage(SCHEDULE)) {
-      "Location attribute with ID ${locationAttribute.locationAttributeId} requires a location usage of SCHEDULE to add a row to it."
-    }
 
     locationAttribute.addSchedule(
       usage = LocationScheduleUsage.valueOf(request.locationUsage!!.name),
