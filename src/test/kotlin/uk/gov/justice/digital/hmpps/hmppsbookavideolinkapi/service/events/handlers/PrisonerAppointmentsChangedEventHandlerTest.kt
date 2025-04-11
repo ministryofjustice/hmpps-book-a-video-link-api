3956debdd7c3619d2132be66b3e816f588f0466a
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-04-11 12:45:39 +0100

    BAVL-752 add telemetry for when a prison chooses to cancel appointments/bookings.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/service/events/handlers/PrisonerAppointmentsChangedEventHandlerTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/service/events/handlers/PrisonerAppointmentsChangedEventHandlerTest.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.events.handl
 
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoInteractions
@@ -10,7 +11,9 @@ import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.client.prisonapi.Pris
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.client.prisonapi.model.Movement
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.config.TimeSource
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.PENTONVILLE
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.RISLEY
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.SERVICE_USER
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.containsEntriesExactlyInAnyOrder
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.courtBooking
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.withMainCourtPrisonAppointment
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.repository.PrisonAppointmentRepository
@@ -19,6 +22,8 @@ import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.events.Appoin
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.events.Identifier
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.events.PersonReference
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.events.PrisonerAppointmentsChangedEvent
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.telemetry.PrisonerAppointmentsCancelledByPrisonTelemetryEvent
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.service.telemetry.TelemetryService
 import java.time.LocalDateTime
 
 class PrisonerAppointmentsChangedEventHandlerTest {
@@ -27,49 +32,63 @@ class PrisonerAppointmentsChangedEventHandlerTest {
   private val bookingFacade: BookingFacade = mock()
   private val timeSource = TimeSource { LocalDateTime.of(2024, 4, 9, 12, 0) }
   private val videoBooking = courtBooking().withMainCourtPrisonAppointment()
-  private val handler = PrisonerAppointmentsChangedEventHandler(prisonAppointmentRepository, prisonApiClient, bookingFacade, timeSource)
+  private val telemetryService: TelemetryService = mock()
+  private val telemetryCaptor = argumentCaptor<PrisonerAppointmentsCancelledByPrisonTelemetryEvent>()
+  private val handler = PrisonerAppointmentsChangedEventHandler(prisonAppointmentRepository, prisonApiClient, bookingFacade, timeSource, telemetryService)
 
   @Test
   fun `should transfer prisoner on cancel`() {
     whenever(prisonAppointmentRepository.findActivePrisonerPrisonAppointmentsAfter("123456", timeSource.today(), timeSource.now().toLocalTime())) doReturn videoBooking.appointments()
     whenever(prisonApiClient.getLatestPrisonerMovementOnDate("123456", timeSource.today())) doReturn Movement.MovementType.TRN
 
-    handler.handle(event(true))
+    handler.handle(event(true, PENTONVILLE, "123456"))
     verify(bookingFacade).prisonerTransferred(videoBooking.videoBookingId, SERVICE_USER)
+    verify(telemetryService).track(telemetryCaptor.capture())
+
+    with(telemetryCaptor.firstValue) {
+      properties() containsEntriesExactlyInAnyOrder mapOf("prisoner_code" to PENTONVILLE, "prisoner_number" to "123456", "reason" to "TRN")
+    }
   }
 
   @Test
   fun `should not transfer prisoner on not cancel`() {
-    handler.handle(event(false))
+    handler.handle(event(false, PENTONVILLE, "123456"))
 
     verifyNoInteractions(prisonAppointmentRepository)
     verifyNoInteractions(prisonApiClient)
     verifyNoInteractions(bookingFacade)
+    verifyNoInteractions(telemetryService)
   }
 
   @Test
   fun `should release prisoner on cancel`() {
-    whenever(prisonAppointmentRepository.findActivePrisonerPrisonAppointmentsAfter("123456", timeSource.today(), timeSource.now().toLocalTime())) doReturn videoBooking.appointments()
-    whenever(prisonApiClient.getLatestPrisonerMovementOnDate("123456", timeSource.today())) doReturn Movement.MovementType.REL
+    whenever(prisonAppointmentRepository.findActivePrisonerPrisonAppointmentsAfter("54321", timeSource.today(), timeSource.now().toLocalTime())) doReturn videoBooking.appointments()
+    whenever(prisonApiClient.getLatestPrisonerMovementOnDate("54321", timeSource.today())) doReturn Movement.MovementType.REL
 
-    handler.handle(event(true))
+    handler.handle(event(true, RISLEY, "54321"))
     verify(bookingFacade).prisonerReleased(videoBooking.videoBookingId, SERVICE_USER)
+    verify(telemetryService).track(telemetryCaptor.capture())
+
+    with(telemetryCaptor.firstValue) {
+      properties() containsEntriesExactlyInAnyOrder mapOf("prisoner_code" to RISLEY, "prisoner_number" to "54321", "reason" to "REL")
+    }
   }
 
   @Test
   fun `should not release prisoner on not cancel`() {
-    handler.handle(event(false))
+    handler.handle(event(false, PENTONVILLE, "123456"))
 
     verifyNoInteractions(prisonAppointmentRepository)
     verifyNoInteractions(prisonApiClient)
     verifyNoInteractions(bookingFacade)
+    verifyNoInteractions(telemetryService)
   }
 
-  private fun event(cancel: Boolean) = PrisonerAppointmentsChangedEvent(
-    personReference = PersonReference(listOf(Identifier("NOMS", "123456"))),
+  private fun event(cancel: Boolean, prisonCode: String, prisonerNumber: String) = PrisonerAppointmentsChangedEvent(
+    personReference = PersonReference(listOf(Identifier("NOMS", prisonerNumber))),
     additionalInformation = AppointmentsChangedInformation(
       action = if (cancel) "YES" else "NO",
-      prisonId = PENTONVILLE,
+      prisonId = prisonCode,
       user = "SOME_USER",
     ),
   )
