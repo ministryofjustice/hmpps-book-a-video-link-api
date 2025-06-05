3d3e8cb4096b2c841945daee4f49fb7d2b647ae7
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-06-05 10:21:57 +0100

    BAVL-913 scripts to enable notes feature toggle and small tweek to info endpoint for feature toggles. (#493)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/health/FeatureToggleContributor.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/health/FeatureToggleContributor.kt
@@ -1,21 +1,20 @@
 package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.health
 
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.actuate.info.Info
 import org.springframework.boot.actuate.info.InfoContributor
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.config.Feature.FEATURE_MASTER_PUBLIC_PRIVATE_NOTES
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.config.FeatureSwitches
 
 @Component
-class FeatureToggleContributor(
-  @Value("\${FEATURE_GREY_RELEASE_PRISONS:default}") private val greyReleasePrisons: String,
-  @Value("\${FEATURE_MASTER_PUBLIC_PRIVATE_NOTES:default}") private val masterPublicPrivateNotes: String,
-) : InfoContributor {
+class FeatureToggleContributor(private val featureSwitches: FeatureSwitches) : InfoContributor {
   override fun contribute(builder: Info.Builder) {
     builder.withDetails(
       mapOf(
         "featureToggles" to mapOf(
-          "FEATURE_GREY_RELEASE_PRISONS" to greyReleasePrisons,
-          "FEATURE_MASTER_PUBLIC_PRIVATE_NOTES" to masterPublicPrivateNotes,
+          FEATURE_MASTER_PUBLIC_PRIVATE_NOTES to featureSwitches.isEnabled(
+            FEATURE_MASTER_PUBLIC_PRIVATE_NOTES,
+          ).toString(),
         ),
       ),
     )
