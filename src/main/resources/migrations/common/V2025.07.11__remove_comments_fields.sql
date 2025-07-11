49e7c0fcf3c63c0d040e99e4c86711f2b3ae32cd
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-07-11 16:09:47 +0100

    BAVL-949 it is now safe to remove the comments field as all data has been migrated accross to the notes_for_staff field. (#516)

---
+++ src/main/resources/migrations/common/V2025.07.11__remove_comments_fields.sql
@@ -0,0 +1,3 @@
+ALTER TABLE video_booking DROP COLUMN IF EXISTS comments;
+ALTER TABLE prison_appointment DROP COLUMN IF EXISTS comments;
+
--- src/test/resources/integration-test-data/seed-events-by-booking-date-data-extract.sql
+++ src/test/resources/integration-test-data/seed-events-by-booking-date-data-extract.sql
@@ -1,22 +1,22 @@
-insert into video_booking (video_booking_id, booking_type, status_code, court_id, hearing_type, comments, created_by, created_time, hmcts_number)
+insert into video_booking (video_booking_id, booking_type, status_code, court_id, hearing_type, notes_for_staff, created_by, created_time, hmcts_number)
 values (-2000, 'COURT', 'ACTIVE', 1, 'TRIBUNAL', 'comments about the hearing', 'court_user', '2024-01-01T01:00:00', '54321');
 
-insert into prison_appointment (video_booking_id, prison_id, prisoner_number, appointment_type, comments, prison_location_id, appointment_date,  start_time, end_time)
+insert into prison_appointment (video_booking_id, prison_id, prisoner_number, appointment_type, notes_for_staff, prison_location_id, appointment_date,  start_time, end_time)
 values (-2000, 1, 'ABCDEF', 'VLB_COURT_MAIN', 'comments about the hearing', '926d8f38-7149-4fda-b51f-85abcbcb0d00'::uuid, '2099-01-24', '12:00', '13:00');
 
-insert into booking_history(booking_history_id, video_booking_id, history_type, court_id, hearing_type, comments, created_by, created_time)
+insert into booking_history(booking_history_id, video_booking_id, history_type, court_id, hearing_type, notes_for_staff, created_by, created_time)
 values (-2000, -2000, 'CREATE', 1, 'TRIBUNAL','comments about the hearing', 'court_user', '2024-01-01T01:00:00');
 
 insert into booking_history_appointment (booking_history_id, prison_code, prisoner_number, appointment_date, appointment_type, prison_location_id, start_time, end_time)
 values (-2000, 'PVI', 'ABCDEF', '2099-01-24', 'VLB_COURT_MAIN', '926d8f38-7149-4fda-b51f-85abcbcb0d00'::uuid, '12:00', '13:00');
 
-insert into video_booking (video_booking_id, booking_type, status_code, probation_team_id, probation_meeting_type, comments, created_by, created_time)
+insert into video_booking (video_booking_id, booking_type, status_code, probation_team_id, probation_meeting_type, notes_for_staff, created_by, created_time)
 values (-3000, 'PROBATION', 'ACTIVE', 1, 'PSR', 'comments about the meeting', 'probation_user', '2024-01-01T01:00:00');
 
-insert into prison_appointment (video_booking_id, prison_id, prisoner_number, appointment_type, comments, prison_location_id, appointment_date,  start_time, end_time)
+insert into prison_appointment (video_booking_id, prison_id, prisoner_number, appointment_type, notes_for_staff, prison_location_id, appointment_date,  start_time, end_time)
 values (-3000, 1, 'ABCDEF', 'VLB_PROBATION', 'comments about the meeting', '926d8f38-7149-4fda-b51f-85abcbcb0d00'::uuid, '2099-01-24', '16:00', '17:00');
 
-insert into booking_history(booking_history_id, video_booking_id, history_type, probation_team_id, probation_meeting_type, comments, created_by, created_time)
+insert into booking_history(booking_history_id, video_booking_id, history_type, probation_team_id, probation_meeting_type, notes_for_staff, created_by, created_time)
 values (-3000, -3000, 'CREATE', 1, 'PSR','comments about the meeting', 'probation_user', '2024-01-01T01:00:00');
 
 insert into booking_history_appointment (booking_history_id, prison_code, prisoner_number, appointment_date, appointment_type, prison_location_id, start_time, end_time)
