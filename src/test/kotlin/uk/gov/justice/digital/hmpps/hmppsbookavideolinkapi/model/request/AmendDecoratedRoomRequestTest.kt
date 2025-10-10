b32c2c9b1ce0355a2c08bca0c3257eee42d4d7f2
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2025-10-10 08:29:50 +0100

    BAVL-1101 relax validation on temporary room blocking start dates. (#563)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/model/request/AmendDecoratedRoomRequestTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/model/request/AmendDecoratedRoomRequestTest.kt
@@ -1,6 +1,9 @@
 package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.model.request
 
 import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.today
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.tomorrow
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.yesterday
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.model.LocationStatus
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.model.LocationUsage
 
@@ -14,6 +17,13 @@ class AmendDecoratedRoomRequestTest : ValidatorBase<AmendDecoratedRoomRequest>()
     comments = "amended comments",
   )
 
+  private val amendBlockedRequest = AmendDecoratedRoomRequest(
+    locationUsage = LocationUsage.SHARED,
+    locationStatus = LocationStatus.TEMPORARILY_BLOCKED,
+    blockedFrom = yesterday(),
+    blockedTo = today(),
+  )
+
   @Test
   fun `should be no errors for valid request`() {
     assertNoErrors(amendRequest)
@@ -22,5 +32,15 @@ class AmendDecoratedRoomRequestTest : ValidatorBase<AmendDecoratedRoomRequest>()
   @Test
   fun `should fail when prison video url exceeds max allowed characters`() {
     amendRequest.copy(prisonVideoUrl = "v".repeat(301)) failsWithSingle ModelError("prisonVideoUrl", "Prison video URL should not exceed 300 characters")
+  }
+
+  @Test
+  fun `should fail when blocked to is in the past`() {
+    amendBlockedRequest.copy(blockedTo = yesterday()) failsWithSingle ModelError("blockedTo", "The blocked to date must be in the future or present")
+  }
+
+  @Test
+  fun `should fail when blocked to is before the blocked from`() {
+    amendBlockedRequest.copy(blockedFrom = tomorrow(), blockedTo = today()) failsWithSingle ModelError("invalidBlocked", "The blocked to must be on or after the blocked from date")
   }
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/model/request/CreateDecoratedRoomRequestTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/model/request/CreateDecoratedRoomRequestTest.kt
@@ -1,12 +1,15 @@
 package uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.model.request
 
 import org.junit.jupiter.api.Test
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.today
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.tomorrow
+import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.helper.yesterday
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.model.LocationStatus
 import uk.gov.justice.digital.hmpps.hmppsbookavideolinkapi.model.LocationUsage
 
 class CreateDecoratedRoomRequestTest : ValidatorBase<CreateDecoratedRoomRequest>() {
 
-  private val createRequest = CreateDecoratedRoomRequest(
+  private val createActiveRequest = CreateDecoratedRoomRequest(
     locationUsage = LocationUsage.COURT,
     locationStatus = LocationStatus.ACTIVE,
     allowedParties = setOf("COURT"),
@@ -14,13 +17,31 @@ class CreateDecoratedRoomRequestTest : ValidatorBase<CreateDecoratedRoomRequest>
     comments = "amended comments",
   )
 
+  private val createBlockedRequest = CreateDecoratedRoomRequest(
+    locationUsage = LocationUsage.SHARED,
+    locationStatus = LocationStatus.TEMPORARILY_BLOCKED,
+    blockedFrom = yesterday(),
+    blockedTo = today(),
+  )
+
   @Test
   fun `should be no errors for valid request`() {
-    assertNoErrors(createRequest)
+    assertNoErrors(createActiveRequest)
+    assertNoErrors(createBlockedRequest)
   }
 
   @Test
   fun `should fail when prison video url exceeds max allowed characters`() {
-    createRequest.copy(prisonVideoUrl = "v".repeat(301)) failsWithSingle ModelError("prisonVideoUrl", "Prison video URL should not exceed 300 characters")
+    createActiveRequest.copy(prisonVideoUrl = "v".repeat(301)) failsWithSingle ModelError("prisonVideoUrl", "Prison video URL should not exceed 300 characters")
+  }
+
+  @Test
+  fun `should fail when blocked to is in the past`() {
+    createBlockedRequest.copy(blockedTo = yesterday()) failsWithSingle ModelError("blockedTo", "The blocked to date must be in the future or present")
+  }
+
+  @Test
+  fun `should fail when blocked to is before the blocked from`() {
+    createBlockedRequest.copy(blockedFrom = tomorrow(), blockedTo = today()) failsWithSingle ModelError("invalidBlocked", "The blocked to must be on or after the blocked from date")
   }
 }
