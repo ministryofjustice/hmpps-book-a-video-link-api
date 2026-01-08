47c0bbef4a72f8280c827c595accca67902ffb00
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2026-01-08 15:05:08 +0000

    BAVL-1187: Retire probation team DEVTCP (#587)

---
+++ src/main/resources/migrations/common/V2026.01.08.1__retire_probation_team.sql
@@ -0,0 +1,6 @@
+-- Retire the probation team code DEVTCP - Devon and Torbay PDU - Court - Probation
+-- The read_only=true indicator prevents it from being shown in A&A and DPS profile prison select lists.
+update probation_team
+set enabled = false,
+    read_only = true
+where code = 'DEVTCP';
