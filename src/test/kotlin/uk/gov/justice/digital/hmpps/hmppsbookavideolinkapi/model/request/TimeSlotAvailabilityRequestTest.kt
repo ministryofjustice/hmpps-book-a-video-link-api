ca4967a217747e03305ba09b6ea5130cfca72327
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-02-26 09:27:14 +0000

    BAVL-1224 relax timeslot duration to allow support multiples of 15 minutes for courts.  Also removed max duration limit. (#609)

--- src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/model/request/TimeSlotAvailabilityRequestTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/model/request/TimeSlotAvailabilityRequestTest.kt
@@ -7,77 +7,61 @@ import java.time.LocalDate
 
 class TimeSlotAvailabilityRequestTest : ValidatorBase<TimeSlotAvailabilityRequest>() {
 
-  private val requestThirty = TimeSlotAvailabilityRequest(
+  private val request = TimeSlotAvailabilityRequest(
     prisonCode = "RSI",
     bookingType = BookingType.PROBATION,
     probationTeamCode = "probation_team_code",
     date = LocalDate.now(),
-    bookingDuration = 30,
+    bookingDuration = 15,
     timeSlots = listOf(TimeSlot.AM),
   )
 
-  private val requestSixty = requestThirty.copy(bookingDuration = 60)
-  private val requestNinety = requestThirty.copy(bookingDuration = 90)
-  private val requestOneTwenty = requestThirty.copy(bookingDuration = 120)
-
   @Test
   fun `should be no errors for valid requests`() {
-    assertNoErrors(requestThirty)
-    assertNoErrors(requestSixty)
-    assertNoErrors(requestNinety)
-    assertNoErrors(requestOneTwenty)
+    assertNoErrors(request)
+
+    // Duration must be a multiple of 15
+    listOf(15, 30, 45, 60, 75, 90, 105, 120, 135, 150, 165, 180).forEach {
+      assertNoErrors(request.copy(bookingDuration = it))
+    }
   }
 
   @Test
   fun `should fail when prison code is missing`() {
-    requestThirty.copy(prisonCode = null) failsWithSingle ModelError("prisonCode", "The prison code is mandatory")
+    request.copy(prisonCode = null) failsWithSingle ModelError("prisonCode", "The prison code is mandatory")
   }
 
   @Test
   fun `should fail when booking type is missing`() {
-    requestThirty.copy(bookingType = null) failsWithSingle ModelError("bookingType", "The booking type is mandatory")
+    request.copy(bookingType = null) failsWithSingle ModelError("bookingType", "The booking type is mandatory")
   }
 
   @Test
   fun `should fail when court code is missing`() {
-    requestThirty.copy(bookingType = BookingType.COURT, courtCode = null) failsWithSingle ModelError("courtBooking", "The court code is mandatory for court bookings")
+    request.copy(bookingType = BookingType.COURT, courtCode = null) failsWithSingle ModelError("courtBooking", "The court code is mandatory for court bookings")
   }
 
   @Test
   fun `should fail when probation team code is missing`() {
-    requestThirty.copy(bookingType = BookingType.PROBATION, probationTeamCode = null) failsWithSingle ModelError("probationBooking", "The probation team code is mandatory for probation bookings")
+    request.copy(bookingType = BookingType.PROBATION, probationTeamCode = null) failsWithSingle ModelError("probationBooking", "The probation team code is mandatory for probation bookings")
   }
 
   @Test
   fun `should fail when no date provided`() {
-    requestThirty.copy(date = null) failsWithSingle ModelError("date", "The date is mandatory")
+    request.copy(date = null) failsWithSingle ModelError("date", "The date is mandatory")
   }
 
   @Test
   fun `should fail when date is in the past`() {
-    requestThirty.copy(date = yesterday()) failsWithSingle ModelError("date", "The date must be future or present")
+    request.copy(date = yesterday()) failsWithSingle ModelError("date", "The date must be future or present")
   }
 
   @Test
   fun `should fail when booking not in allowed slots`() {
-    IntRange(1, 29).forEach {
-      requestThirty.copy(bookingDuration = it) failsWithSingle ModelError("allowedDuration", "The booking duration can only be one of 30, 60, 90 or 120 minutes")
+    IntRange(1, 1000).filterNot { it.mod(15) == 0 }.forEach {
+      request.copy(bookingDuration = it) failsWithSingle ModelError("allowedDuration", "The booking duration must be a multiple of 15 minutes")
     }
 
-    IntRange(31, 59).forEach {
-      requestThirty.copy(bookingDuration = it) failsWithSingle ModelError("allowedDuration", "The booking duration can only be one of 30, 60, 90 or 120 minutes")
-    }
-
-    IntRange(61, 89).forEach {
-      requestThirty.copy(bookingDuration = it) failsWithSingle ModelError("allowedDuration", "The booking duration can only be one of 30, 60, 90 or 120 minutes")
-    }
-
-    IntRange(91, 119).forEach {
-      requestThirty.copy(bookingDuration = it) failsWithSingle ModelError("allowedDuration", "The booking duration can only be one of 30, 60, 90 or 120 minutes")
-    }
-
-    requestThirty.copy(bookingDuration = 121) failsWithSingle ModelError("allowedDuration", "The booking duration can only be one of 30, 60, 90 or 120 minutes")
-
-    requestThirty.copy(bookingDuration = null) failsWithSingle ModelError("bookingDuration", "The booking duration is mandatory")
+    request.copy(bookingDuration = null) failsWithSingle ModelError("bookingDuration", "The booking duration is mandatory")
   }
 }
