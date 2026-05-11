1ea1809cb1eba2f972cae7d9bfbe37a891188081
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-05-11 13:50:09 +0100

    BAVL-1329 add rollout script to assist with rolling out changes to secrets.

---
+++ utility/rollout-control.sh
@@ -0,0 +1,274 @@
+#!/bin/bash
+#
+# Script to manage the rollout options in Kubernetes secrets.
+# ENV should be specified in lowercase, either dev, preprod or prod.
+#
+
+menu_function() {
+  echo " BVLS admin email addresses - changes require API restart only"
+  echo ""
+  echo " 1 - Replace with a new list"
+  echo " 2 - Add an email address"
+  echo " 3 - Remove an email address"
+  echo ""
+  echo " Grey release prisons - changes require UI restart only"
+  echo ""
+  echo " 4 - Replace with a new list"
+  echo " 5 - Add a grey release prison"
+  echo " 6 - Remove a grey release prison"
+  echo ""
+  echo " Probation only prisons - changes require UI restart only"
+  echo ""
+  echo " 7 - Replace with a new list"
+  echo " 8 - Add a probation only prison"
+  echo " 9 - Remove a probation only prison"
+  echo ""
+  echo " Court only prisons - changes require UI restart only"
+  echo ""
+  echo " 10 - Replace with a new list"
+  echo " 11 - Add a court only prison"
+  echo " 12 - Remove a court only prison"
+  echo ""
+  echo " Restart services"
+  echo ""
+  echo " 13 - Restart BVLS UI for changes to take effect"
+  echo " 14 - Restart BVLS API for changes to take effect"
+  echo " 15 - Restart services for changes to take effect"
+  echo ""
+  echo " 0 - Exit"
+  echo "----------------------------"
+}
+
+show_current() {
+  NAMESPACE=$1
+  ENV=$2
+
+  echo "Getting secrets from $ENV ..."
+
+  ADMINISTRATION_EMAILS=$(kubectl -n "$NAMESPACE" get secret administration -o jsonpath='{.data.ADMINISTRATION_EMAILS}' | base64 -d)
+  FEATURE_GREY_RELEASE_PRISONS=$(kubectl -n "$NAMESPACE" get secret feature-toggles -o jsonpath='{.data.FEATURE_GREY_RELEASE_PRISONS}' | base64 -d)
+  FEATURE_PROBATION_ONLY_PRISONS=$(kubectl -n "$NAMESPACE" get secret feature-toggles -o jsonpath='{.data.FEATURE_PROBATION_ONLY_PRISONS}' | base64 -d)
+  FEATURE_COURT_ONLY_PRISONS=$(kubectl -n "$NAMESPACE" get secret feature-toggles -o jsonpath='{.data.FEATURE_COURT_ONLY_PRISONS}' | base64 -d)
+
+  clear
+  echo "-------------------------------------------------------------------------------------"
+  echo "Environment                 : $ENV"
+  echo ""
+  echo "BVLS admin emails           : $ADMINISTRATION_EMAILS"
+  echo "Grey release prisons        : $FEATURE_GREY_RELEASE_PRISONS"
+  echo "Probation only prisons      : $FEATURE_PROBATION_ONLY_PRISONS"
+  echo "Court only prisons          : $FEATURE_COURT_ONLY_PRISONS"
+  echo ""
+}
+
+add_list_bvls_admin_emails() {
+  echo "Replace existing list with $3 for DPS enabled prisons in $1 namespace $2"
+  CURRENT=$(kubectl -n "$2" get secret administration -o jsonpath='{.data.ADMINISTRATION_EMAILS}' | base64 -d)
+  NEW=$3
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"ADMINISTRATION_EMAILS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret administration -p $stringData
+}
+
+add_bvls_admin_email() {
+  echo "Adding $3 to BVLS admin emails in $1 namespace $2"
+  CURRENT=$(kubectl -n "$2" get secret administration -o jsonpath='{.data.ADMINISTRATION_EMAILS}' | base64 -d)
+  NEW="$CURRENT,$3"
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"ADMINISTRATION_EMAILS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret administration -p $stringData
+}
+
+remove_bvls_admin_email() {
+  echo "Removing prison $3 from DPS enabled prisons in $1 namespace $2"
+  prison=$3
+  CURRENT=$(kubectl -n "$2" get secret administration -o jsonpath='{.data.ADMINISTRATION_EMAILS}' | base64 -d)
+  NEW=$(echo ",$CURRENT," | sed "s/,$prison,/,/g; s/^,//; s/,$//")
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"ADMINISTRATION_EMAILS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret administration -p $stringData
+}
+
+add_list_grey_release_prison() {
+  echo "Replace existing list with $3 for grey release prisons in $1 namespace $2"
+  CURRENT=$(kubectl -n "$2" get secret feature-toggles -o jsonpath='{.data.FEATURE_GREY_RELEASE_PRISONS}' | base64 -d)
+  NEW=$3
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"FEATURE_GREY_RELEASE_PRISONS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret feature-toggles -p $stringData
+}
+
+add_grey_release_prison() {
+  echo "Adding $3 to grey release prisons in $1 namespace $2"
+  CURRENT=$(kubectl -n "$2" get secret feature-toggles -o jsonpath='{.data.FEATURE_GREY_RELEASE_PRISONS}' | base64 -d)
+  NEW="$CURRENT,$3"
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"FEATURE_GREY_RELEASE_PRISONS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret feature-toggles -p $stringData
+}
+
+remove_grey_release_prison() {
+  echo "Removing prison $3 from grey release prisons in $1 namespace $2"
+  prison=$3
+  CURRENT=$(kubectl -n "$2" get secret feature-toggles -o jsonpath='{.data.FEATURE_GREY_RELEASE_PRISONS}' | base64 -d)
+  NEW=$(echo ",$CURRENT," | sed "s/,$prison,/,/g; s/^,//; s/,$//")
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"FEATURE_GREY_RELEASE_PRISONS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret feature-toggles -p $stringData
+}
+
+add_list_probation_only_prison() {
+  echo "Replace existing list with $3 for probation only prisons in $1 namespace $2"
+  CURRENT=$(kubectl -n "$2" get secret feature-toggles -o jsonpath='{.data.FEATURE_PROBATION_ONLY_PRISONS}' | base64 -d)
+  NEW=$3
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"FEATURE_PROBATION_ONLY_PRISONS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret feature-toggles -p $stringData
+}
+
+add_probation_only_prison() {
+  echo "Adding $3 to probation only prisons in $1 namespace $2"
+  CURRENT=$(kubectl -n "$2" get secret feature-toggles -o jsonpath='{.data.FEATURE_PROBATION_ONLY_PRISONS}' | base64 -d)
+  NEW="$CURRENT,$3"
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"FEATURE_PROBATION_ONLY_PRISONS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret feature-toggles -p $stringData
+}
+
+remove_probation_only_prison() {
+  echo "Removing prison $3 from probation only prisons in $1 namespace $2"
+  prison=$3
+  CURRENT=$(kubectl -n "$2" get secret feature-toggles -o jsonpath='{.data.FEATURE_PROBATION_ONLY_PRISONS}' | base64 -d)
+  NEW=$(echo ",$CURRENT," | sed "s/,$prison,/,/g; s/^,//; s/,$//")
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"FEATURE_PROBATION_ONLY_PRISONS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret feature-toggles -p $stringData
+}
+
+add_list_court_only_prison() {
+  echo "Replace existing list with $3 for court only prisons in $1 namespace $2"
+  CURRENT=$(kubectl -n "$2" get secret feature-toggles -o jsonpath='{.data.FEATURE_COURT_ONLY_PRISONS}' | base64 -d)
+  NEW=$3
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"FEATURE_COURT_ONLY_PRISONS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret feature-toggles -p $stringData
+}
+
+add_court_only_prison() {
+  echo "Adding $3 to court only prisons in $1 namespace $2"
+  CURRENT=$(kubectl -n "$2" get secret feature-toggles -o jsonpath='{.data.FEATURE_COURT_ONLY_PRISONS}' | base64 -d)
+  NEW="$CURRENT,$3"
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"FEATURE_COURT_ONLY_PRISONS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret feature-toggles -p $stringData
+}
+
+remove_court_only_prison() {
+  echo "Removing prison $3 from court only prisons in $1 namespace $2"
+  prison=$3
+  CURRENT=$(kubectl -n "$2" get secret feature-toggles -o jsonpath='{.data.FEATURE_COURT_ONLY_PRISONS}' | base64 -d)
+  NEW=$(echo ",$CURRENT," | sed "s/,$prison,/,/g; s/^,//; s/,$//")
+  echo "Applying new value : $NEW"
+  stringData="{\"stringData\":{\"FEATURE_COURT_ONLY_PRISONS\":\"$NEW\"}}"
+  kubectl -n "$2" patch secret feature-toggles -p $stringData
+}
+
+restart_bvls_ui() {
+   echo "Restarting BVLS UI service in $1 namespace $2"
+   kubectl -n "$2" rollout restart deployments/hmpps-book-a-video-link-ui
+}
+
+restart_bvls_api() {
+   echo "Restarting BVLS API service in $1 namespace $2"
+   kubectl -n "$2" rollout restart deployments/hmpps-book-a-video-link-api
+}
+
+restart_all_services() {
+   restart_bvls_ui "$1" "$2"
+   restart_bvls_api "$1" "$2"
+}
+
+ENV=$1
+NAMESPACE="hmpps-book-a-video-link-$ENV"
+
+# Temporarily disable any prod runs
+if [ "$ENV" = "prod" ]; then
+  echo ""
+  echo "Prod is currently disabled, you must comment this check out of the script to bypass."
+  echo ""
+  return 1 2> /dev/null || exit 1
+fi
+
+while true; do
+  show_current "$NAMESPACE" "$ENV"
+  menu_function "$ENV"
+  read -p "Select an option: " choice
+
+  case $choice in
+      1)  echo "Replace BVLS admin emails with a new list"
+          read -p "Enter a comma-separated list of emails to replace the current list : " email_list
+          add_list_bvls_admin_emails "$ENV" "$NAMESPACE" "$email_list"
+          ;;
+      2)  echo "Add an email to the admin emails list"
+          read -p "Enter an email to add : " email
+          add_bvls_admin_email "$ENV" "$NAMESPACE" "$email"
+          ;;
+      3)  echo "Remove an email from the admin emails list"
+          read -p "Enter an email to remove : " email
+          remove_bvls_admin_email "$ENV" "$NAMESPACE" "$email"
+          ;;
+      4)  echo "Replace grey release prisons with a new list"
+          read -p "Enter a comma-separated list of grey prisons to replace the current list : " grey_prison_list
+          add_list_grey_release_prison "$ENV" "$NAMESPACE" "$grey_prison_list"
+          ;;
+      5)  echo "Add a prison to the grey release prisons list"
+          read -p "Enter a grey prison to add : " grey_prison
+          add_grey_release_prison "$ENV" "$NAMESPACE" "$grey_prison"
+          ;;
+      6)  echo "Remove a prison from the grey release prisons list"
+          read -p "Enter a grey prison to remove : " grey_prison
+          remove_grey_release_prison "$ENV" "$NAMESPACE" "$grey_prison"
+          ;;
+      7)  echo "Replace probation only prisons with a new list"
+          read -p "Enter a comma-separated list of probation only prisons to replace the current list : " probation_only_prison_list
+          add_list_probation_only_prison "$ENV" "$NAMESPACE" "$probation_only_prison_list"
+          ;;
+      8)  echo "Add a prison to the probation only prisons list"
+          read -p "Enter a probation only prison to add : " probation_only_prison
+          add_probation_only_prison "$ENV" "$NAMESPACE" "$probation_only_prison"
+          ;;
+      9)  echo "Remove a prison from the probation only prisons list"
+          read -p "Enter a probation only prison to remove : " probation_only_prison
+          remove_probation_only_prison "$ENV" "$NAMESPACE" "$probation_only_prison"
+          ;;
+      10)  echo "Replace court only prisons with a new list"
+          read -p "Enter a comma-separated list of court only prisons to replace the current list : " court_only_prison_list
+          add_list_court_only_prison "$ENV" "$NAMESPACE" "$court_only_prison_list"
+          ;;
+      11)  echo "Add a prison to the court only prisons list"
+          read -p "Enter a court only prison to add : " court_only_prison
+          add_court_only_prison "$ENV" "$NAMESPACE" "$court_only_prison"
+          ;;
+      12)  echo "Remove a prison from the court only prisons list"
+          read -p "Enter a court only prison to remove : " court_only_prison
+          remove_court_only_prison "$ENV" "$NAMESPACE" "$court_only_prison"
+          ;;
+      13) echo "Restarting BVLS UI"
+          restart_bvls_ui "$ENV" "$NAMESPACE"
+          ;;
+      14) echo "Restarting BVLS API"
+          restart_bvls_api "$ENV" "$NAMESPACE"
+          ;;
+      15) echo "Restarting all services"
+          restart_all_services "$ENV" "$NAMESPACE"
+          ;;
+      0)  echo "Exiting..."
+          exit 0
+          ;;
+      *)  echo "Invalid selection. Please try again."
+          ;;
+  esac
+  echo ""
+done
+
+# End
