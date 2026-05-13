61a045efc1644ee50f2e6838b3b26a1f8539b445
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2026-05-13 12:01:13 +0100

    BAVL-1291: New social security tribunal courts added for the North West (#650)

---
+++ src/main/resources/migrations/common/V2026.05.12__add_social_security_tribunals.sql
@@ -0,0 +1,12 @@
+insert into court (code, description, enabled, read_only, notes, created_by, created_time)
+values ('CRLSSC', 'Carlisle Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('BRWSSC', 'Barrow Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('BRKSSC', 'Birkenhead Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('CHSSSC', 'Chester Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('LANSSC', 'Lancaster Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('MANSSC', 'Manchester Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('PRSSSC', 'Preston Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('SHESSC', 'St Helens Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('STKSSC', 'Stockport Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('WIGSSC', 'Wigan Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp),
+       ('WRKSSC', 'Workington Social Security and Child Support Tribunal', true, false, 'Not in register', 'TIM', current_timestamp);
