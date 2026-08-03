78618e32084ed50a2676c6b3a50471e6a5a98420
timharrison-moj <47779388+timharrison-moj@users.noreply.github.com>
2026-08-03 12:59:42 +0100

    BAVL-1425: Adding south central probation teams (#693)

---
+++ src/main/resources/migrations/common/V2026.08.03__south_central_probation_teams.sql
@@ -0,0 +1,21 @@
+-- South Central probation teams
+insert into probation_team (code, description, enabled, read_only, notes, created_by, created_time, court_team)
+values
+    ('ALDSMC', 'Aldershot Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('AMRSCC', 'Amersham Crown – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('AYLSCC', 'Aylesbury Crown – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('BASIMC', 'Basingstoke Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('HYWCMC', 'High Wycombe Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('IOWGCC', 'Isle of Wight Crown – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('IOWGMC', 'Isle of Wight Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('MIKYMC', 'Milton Keynes Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('OXFDCC', 'Oxford Crown – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('OXFDMC', 'Oxfordshire Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('PORTCC', 'Portsmouth Crown – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('PORTMC', 'Portsmouth Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('RDNGCC', 'Reading Crown – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('RDNGMC', 'Reading Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('SLGHMC', 'Slough Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('SOTMCC', 'Southampton Crown – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('SOTMMC', 'Southampton Magistrates – Probation', true, false, null, 'TIM', current_timestamp, true),
+    ('WNCSCC', 'Winchester Crown – Probation', true, false, null, 'TIM', current_timestamp, true);
