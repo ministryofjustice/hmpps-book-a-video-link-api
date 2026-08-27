6d0bb0352e64ae5a6b5ac1d2c2f4dbbabc06aacf
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2026-08-27 18:24:18 +0100

    BAVL-1449: Update minor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ redacted {
 dependencies {
 
   // HMPPS dependencies
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:3.0.0")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.4.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:3.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:7.4.1")
 
   // Spring boot dependencies
   implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -76,7 +76,7 @@ dependencies {
 
   testImplementation("org.wiremock:wiremock-standalone:3.13.2")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.43") {
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.45") {
     exclude(group = "io.swagger.core.v3")
   }
 }
