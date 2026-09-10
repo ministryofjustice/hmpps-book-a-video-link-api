96fff6956586dcc15c91fe5d41b916d2d6a7a7cf
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2026-09-10 17:04:48 +0100

    BAVL-1467: Added filter to lower-priority options with same times.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/service/locations/availability/TimeSlotAvailabilityService.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/hmppsbookavideolinkapi/service/locations/availability/TimeSlotAvailabilityService.kt
@@ -181,7 +181,7 @@ class TimeSlotLocationsBuilder private constructor() : LocationAvailabilityServi
 
     val probation = buildList {
       addAll(specifiedLocations)
-      addAll(dedicatedProbationTeamLocations)
+      addAll(dedicatedProbationTeamLocations.filter { that -> filter(this, that) })
       addAll(probationCourtTeamLocations.filter { that -> filter(this, that) })
       addAll(probationSentenceTeamLocations.filter { that -> filter(this, that) })
       addAll(anyProbationTeamLocations.filter { that -> filter(this, that) })
